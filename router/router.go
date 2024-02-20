daf753aa2afdd969eb17858be54b1aafff417f3a
Gustavo Pedrosa <luiz.gustavo.pedrosa@gmail.com>
2024-02-20 18:01:45 -0300

    feat: create opening routes

--- router/router.go
+++ router/router.go
@@ -2,14 +2,11 @@ package router
 
 import "github.com/gin-gonic/gin"
 
+// Initialize initializes and run the router.
 func Initialize() {
 	router := gin.Default()
 
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	initializeRoutes(router)
 
-	router.Run() // listen and serve on 0.0.0.0:8080
+	router.Run()
 }
